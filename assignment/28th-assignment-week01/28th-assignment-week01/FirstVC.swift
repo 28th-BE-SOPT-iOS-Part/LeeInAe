6c5ebbcfcd7919bc54d237aeaef2f4892d8932fd
inaeLee <inae9709@hanmail.net>
2021-04-10 17:31:09 +0900

    add: 2주차 세미나

--- assignment/28th-assignment-week01/28th-assignment-week01/FirstVC.swift
+++ assignment/28th-assignment-week01/28th-assignment-week01/FirstVC.swift
@@ -15,16 +15,9 @@ class FirstVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        NSLog("segue : \(segue.identifier)")
     }
-    */
 
     @IBAction func wind(_ sender: Any) {
         self.performSegue(withIdentifier: "manualSegue", sender: self)
---
+++ seminar/28th-seminar-week02/28th-seminar-week02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  28th-seminar-week02
+//
+//  Created by inae Lee on 2021/04/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
